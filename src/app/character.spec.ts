b608174bcf44fbac55fc5b146349c4b491f38afd
Erin Bergman <ebergman@pillartechnology.com>
2017-05-12 09:57:05 -0400

    Refactor character test

--- src/app/character.spec.ts
+++ src/app/character.spec.ts
@@ -2,19 +2,21 @@ import { expect } from 'chai';
 import { Character } from './character';
 
 describe('Character Spec', () => {
-  describe('Character name', () => {
-      it('should get a character name', () => {
-          const testCharacter = new Character();
-          testCharacter.name = 'Leeana';
-          expect(testCharacter.name).to.equal('Leeana');
-      });
-  });
+    let testCharacter: Character;
 
-  describe('Character name', () => {
-      it('should set a character name', () => {
-          const testCharacter = new Character();
-          testCharacter.name = 'Inara';
-          expect(testCharacter.name).to.equal('Inara');
-      });
-  });
+    beforeEach(() => {
+        testCharacter = new Character();
+    });
+
+    describe('Character name', () => {
+        it('should get a character name', () => {
+            testCharacter.name = 'Leeana';
+            expect(testCharacter.name).to.equal('Leeana');
+        });
+
+        it('should set a character name', () => {
+            testCharacter.name = 'Inara';
+            expect(testCharacter.name).to.equal('Inara');
+        });
+    });
 });
