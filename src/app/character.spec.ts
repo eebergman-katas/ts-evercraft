5e2ed54e94e35acf5c673aa832891405be4908b7
Erin Bergman <ebergman@pillartechnology.com>
2017-05-12 10:31:49 -0400

    Add alignement lookup method, refactor

--- src/app/character.spec.ts
+++ src/app/character.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+
 import { Character } from './character';
 
 describe('Character Spec', () => {
@@ -17,6 +18,23 @@ describe('Character Spec', () => {
         it('should set a character name', () => {
             testCharacter.name = 'Inara';
             expect(testCharacter.name).to.equal('Inara');
+        });
+    });
+
+    describe('Character Alignment', () => {
+        it('should get a character Alignment', () => {
+            testCharacter.alignment = 0;
+            expect(testCharacter.alignment).to.equal('Good');
+        });
+
+        it('should get a character Alignment', () => {
+            testCharacter.alignment = 2;
+            expect(testCharacter.alignment).to.equal('Evil');
+        });
+
+        it('should look up the alignment', () => {
+            testCharacter.alignment = 1;
+            expect(testCharacter.alignment).to.equal('Neutral');
         });
     });
 });
--- src/app/character.ts
+++ src/app/character.ts
@@ -1,5 +1,8 @@
+import { Alignment } from './shared/alignment.enum';
+
 export class Character {
     private _name: string;
+    private _alignment: string;
 
     get name() {
         return this._name;
@@ -7,5 +10,20 @@ export class Character {
 
     set name(name: string) {
         this._name = name;
+    }
+
+    get alignment() {
+        return this._alignment;
+    }
+
+    set alignment(alignmentKey: number | string) {
+        this._alignment = this.lookUpAlignment(alignmentKey);
+    }
+
+    private lookUpAlignment(alignmentKey: number | string): string {
+        if (typeof alignmentKey === 'number') {
+            return Alignment[alignmentKey];
+        }
+        return alignmentKey;
     }
 }
